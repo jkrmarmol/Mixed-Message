0946c57deecf98bf1266415f9f30b254e4ba94f3
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2022-05-26 14:08:23 +0800

    Message Added

---
+++ main.js
@@ -0,0 +1,1 @@
+const arrMessage = ['When you have a dream, you\'ve got to grab it and never let go.', 'Nothing is impossible.', 'There is nothing impossible to they who will try', 'The bad news is time flies.']
