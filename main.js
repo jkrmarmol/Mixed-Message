b75cff035ebf80c828faa9d537fec4ab56d8fa61
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2022-05-26 14:09:05 +0800

    Added Return Random Message

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 const arrMessage = ['When you have a dream, you\'ve got to grab it and never let go.', 'Nothing is impossible.', 'There is nothing impossible to they who will try', 'The bad news is time flies.']
+
+const randMessage = () => {
+    let randomNum = Math.floor(Math.random() * arrMessage.length);
+    return arrMessage[randomNum];
+}
